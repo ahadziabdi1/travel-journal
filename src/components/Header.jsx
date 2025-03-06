45c555db0a51499cb1dd08e620659e6e99c26acc
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2025-03-06 21:13:55 +0100

    refactor: restructure App component and add Entry and Header components; include new images and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 function Header() {
-  return <>
-  <h1>my travel journal</h1>
-  </>;
+  return (
+    <>
+      <header>
+        <img src="src/globus-icon.jpg" alt="globe Icon"/>
+        <h1>my travel journal.</h1>
+      </header>
+    </>
+  );
 }
 
 export default Header;
